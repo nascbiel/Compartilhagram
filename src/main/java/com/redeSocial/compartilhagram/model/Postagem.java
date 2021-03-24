4ca6c2b25ccc41634258a99aa6d4e7427e58a1b5
DanielFilho2000 <daniel.agff@gmail.com>
2021-03-24 11:00:30 -0300

    Task 1 criação de models

---
+++ src/main/java/com/redeSocial/compartilhagram/model/Postagem.java
@@ -0,0 +1,49 @@
+package com.redeSocial.compartilhagram.model;
+
+import java.util.Date;
+
+public class Postagem {
+	
+	private long id;
+	
+	private String imagem;
+
+	private String texto;
+	
+	private Date date;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public String getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+	
+	
+
+}
---
+++ src/test/java/com/redeSocial/compartilhagram/CompartilhagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redeSocial.compartilhagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompartilhagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
